a9c941bb5ea9697cd435190f424aa01cf6626f0b
bscho99 <bscho99@kaist.ac.kr>
2024-09-27 17:15:25 +0900

    Main 구현

--- RecursiveDescentParser/Program.cs
+++ RecursiveDescentParser/Program.cs
@@ -1,9 +1,53 @@
 ﻿namespace RecursiveDescentParser;
 
-class Program
+static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var scanner = new Scanner();
+        var parser = new Parser();
+
+        while (true)
+        {
+            Console.WriteLine("Enter expression. Type 'exit' to exit.");
+            var expressionStr = Console.ReadLine();
+
+            if (expressionStr is null)
+            {
+                continue;
+            }
+
+            if (expressionStr == "exit")
+            {
+                break;
+            }
+            
+            scanner.Load(expressionStr);
+            IList<Token> tokens;
+
+            try
+            {
+                tokens = scanner.Tokenize();
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+                continue;
+            }
+            
+            parser.Load(tokens);
+
+            try
+            {
+                var tree = parser.Parse();
+                Console.WriteLine("Parsed expression: ");
+                Console.WriteLine(tree.PreOrder());
+                Console.WriteLine();
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
     }
 }
