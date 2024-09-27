29a2c960b6650934a69e8550efe829942cd599d8
bscho99 <bscho99@kaist.ac.kr>
2024-09-27 16:20:10 +0900

    TokenTest 추가

---
+++ RecursiveDescentParserTest/TokenTest.cs
@@ -0,0 +1,22 @@
+﻿using RecursiveDescentParser;
+
+namespace RecursiveDescentParserTest;
+
+[TestFixture]
+public class TokenTest
+{
+    [Test]
+    public void Test_IsOperator()
+    {
+        var plus = new Token(TokenType.Operator, "+");
+        var minus = new Token(TokenType.Operator, "-");
+        var mul = new Token(TokenType.Operator, "*");
+        var div = new Token(TokenType.Operator, "/");
+        
+        Assert.That(plus.IsPlus(), Is.True);
+        Assert.That(plus.IsMinus, Is.False);
+        Assert.That(div.IsDiv, Is.True);
+        Assert.That(mul.IsMul, Is.True);
+        Assert.That(minus.IsMinus, Is.True);
+    }
+}
